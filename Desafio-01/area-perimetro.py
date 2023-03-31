2cd69ef8ee157aa431f5817c483a3a260dd596a9
OGabrielPereira <OGabrielPereira@pm.me>
2023-03-31 09:33:55 -0300

    Desafio

---
+++ Desafio-01/area-perimetro.py
@@ -0,0 +1,4 @@
+# Faça um algoritmo para ler o valor do lado de uma quadrado e mostrar sua área e seu perímetro.
+
+lado = float(input("Valor de um lado do quadrado: "))
+print(f"A área é igual a {lado ** 2} e o perímetro é igual a {lado * 4}")
---
+++ Desafio-01/qt-filhos.py
@@ -0,0 +1,3 @@
+nome = input("Digite seu nome: ")
+qtFilhos = int(input("Digite quantos filhos você tem: "))
+print(f"{nome} tem {qtFilhos} {('filho' if qtFilhos == 1 else 'filhos')}")
